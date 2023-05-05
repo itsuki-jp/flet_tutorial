80417377122a0d0909db2e20277d79a0dbad7c2c
itsuki-jp <itsuki19601@gmail.com>
2023-05-05 09:23:11 +0900

    DONE: rewrite into class

--- todo_app/todo.py
+++ todo_app/todo.py
@@ -1,28 +1,37 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        page.update()
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(hint_text="Whats need to be done?", expand=True)
+        self.tasks = ft.Column()
 
-    new_task = ft.TextField(hint_text="Whats need to be done?", expand=True)
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(
+                            icon=ft.icons.ADD, on_click=self.add_clicked
+                        ),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
+def main(page: ft.Page):
+    page.title = "Todo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+    page.add(TodoApp())
 
 
 ft.app(target=main)
